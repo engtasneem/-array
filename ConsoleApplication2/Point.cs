636c54733dfa950e35ac3375aa2a1ef7aa6fb80e
engtasneem <134233991+engtasneem@users.noreply.github.com>
2023-05-22 21:06:01 +0300

    Add files via upload

---
+++ ConsoleApplication2/Point.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        public Point (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+        public void Move()
+        {
+            x = x - 5;
+            y = y + 5;
+            Console.WriteLine(  "The point Move"+"("+x +" ,"+ y+")");
+
+        }
+        public void Quadrant()
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("first quarter");
+            }
+            if (x < 0 && y > 0)
+            {
+                Console.WriteLine("sconad quarter");
+            }
+            if (x <0 && y < 0)
+            {
+                Console.WriteLine("third quarter");
+            }
+            if (x > 0 && y < 0)
+            {
+                Console.WriteLine("fourth quarter");
+            }
+
+        }
+        public void print()
+        {
+            Console.WriteLine("The print point"+"(" + x + "," + y + ")");
+        }
+        
+
+    }
+
+
+    }
+
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point r = new Point(9, 6);
+            r.Move();
+            r.print();
+            r.Quadrant();
+
+        }
+    }
+}
